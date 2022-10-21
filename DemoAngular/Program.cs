e24d47f2fc38e92c847d515931b4ca9cc15c9645
NORBIT\Nafisa.Zakieva <nafisa.zakieva@norbit.ru>
2022-10-21 13:19:49 +0300

    +

--- DemoAngular/Program.cs
+++ DemoAngular/Program.cs
@@ -1,13 +1,36 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Reflection;
 
 namespace DemoAngular
 {
+    class myClass
+    {
+        public string NameField;
+
+        public string NameProp { get; set; }
+
+        public string GetName()
+        {
+            return ToString();
+        }
+    }
+
     public class Program
     {
+        private static void test()
+        {
+            Type myType = typeof(myClass);
+            var fields = myType.GetFields();
+            var properties = myType.GetProperties();
+            var methods = myType.GetMethods();
+        }
+
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            test();
+            //CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
